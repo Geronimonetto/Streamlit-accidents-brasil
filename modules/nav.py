33e1e25c7f6154afce447ac50765682c7f423950
Geronimonetto <geronimomorais1617@gmail.com>
2025-02-17 21:20:25 -0300

    release: Removendo paginas sem necessidade

--- modules/nav.py
+++ modules/nav.py
@@ -3,4 +3,3 @@ import streamlit as st
 def navbar():
     with st.sidebar:
         st.page_link('streamlit_app.py', label='Acidentes', icon='🚗')  # Ícone de carro para representar acidentes
-        st.page_link('pages/mortalidade.py', label='Mortalidade', icon='⚰️')  # Ícone de caixão para representar fatalidades
